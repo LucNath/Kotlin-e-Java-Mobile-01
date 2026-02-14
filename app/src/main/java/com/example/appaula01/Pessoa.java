b8c2df7d14a9b0cc02d43ed96b645bbabf4c00f7
LucNath <lucasnathan.marc@gmail.com>
2026-02-14 01:31:47 -0300

    Adicionando Classes no Projeto.

--- app/src/main/java/com/example/appaula01/Pessoa.java
+++ app/src/main/java/com/example/appaula01/Pessoa.java
@@ -12,4 +12,25 @@ public class Pessoa {
 
     }
 
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + nome + "\nIdade: " + idade;
+    }
+
 }
